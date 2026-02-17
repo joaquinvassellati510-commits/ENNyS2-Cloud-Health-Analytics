133fc35ab37bde717ece017118a100d75b20c654
joaquinvassellati510-commits <joaquinvassellati510@gmail.com>
2026-02-17 13:00:19 -0300

    Update 01_kpis_generales.sql

--- sql/01_kpis_generales.sql
+++ sql/01_kpis_generales.sql
@@ -1,1 +1,11 @@
-vxvcxvx
+--KPIS GENERALES ADULTOS ENNyS2
+
+SELECT
+  COUNT(*) AS total_registros,
+  ROUND(AVG(IMC),2) AS imc_promedio
+FROM ennys2-health-analytics.ennys2.adultos
+
+EXCESO DE PESO
+SELECT
+  ROUND(AVG(exceso_peso)*100,2) AS prevalencia_exceso_peso
+FROM ennys2-health-analytics.ennys2.adultos
