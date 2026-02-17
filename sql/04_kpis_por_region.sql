f92dbabae7c05eb7ef2864f11378322d0d5babc2
joaquinvassellati510-commits <joaquinvassellati510@gmail.com>
2026-02-17 13:25:00 -0300

    Create 04_kpis_por_region.sql

---
+++ sql/04_kpis_por_region.sql
@@ -0,0 +1,12 @@
+-- Exceso de peso por nivel educativo
+
+SELECT
+  Nivel_educacion_total,
+  ROUND(AVG(exceso_peso)*100,2) AS prevalencia_exceso_peso,
+  ROUND(AVG(obesidad)*100,2) AS prevalencia_obesidad,
+  COUNT(*) AS n
+FROM ennys2-health-analytics.ennys2.adultos
+WHERE Nivel_educacion_total IS NOT NULL
+  AND TRIM(Nivel_educacion_total) != ''
+GROUP BY Nivel_educacion_total
+ORDER BY prevalencia_obesidad DESC;
